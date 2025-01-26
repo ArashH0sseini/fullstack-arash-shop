965d909c5d6f89590da31c4fd6b3d3388afb9901
Arash Hosseini <arashhosseini733@gmail.com>
2025-01-26 11:48:12 +0330

    set up context structure for state management

---
+++ frontend/src/context/ShoppingCartContext.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+type ShoppingCartContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type CartItems = {
+  id: number;
+  qty: number;
+};
+
+const ShoppingCartContext = createContext({});
+
+export function ShoppingCartContextProvider({
+  children,
+}: ShoppingCartContextProviderProps) {
+  const [cartItems] = useState<CartItems[]>([]);
+
+  return (
+    <ShoppingCartContext.Provider value={{ cartItems }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
